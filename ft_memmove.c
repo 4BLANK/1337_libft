e8f8f3030de1e2915291eb63fe56f149f548a7a5
amasdouq <amasdouq@c4r3p5.1337.ma>
2023-11-04 21:56:51 +0100

    Add ft_memmove and ft_memcpy

---
+++ ft_memmove.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amasdouq <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 21:12:53 by amasdouq          #+#    #+#             */
+/*   Updated: 2023/11/04 21:55:18 by amasdouq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+void  *ft_memmove(void *dest, const void *src, size_t n)
+{
+  size_t i;
+
+  i = 0;
+  if (src > dest)
+  {
+    while (i < n)
+    {
+      ((unsigned char*)dest)[i] = ((unsigned char*)src)[i];
+      i++;
+    }
+  }
+  if (dest > src)
+  {
+    while (i < n - 1)
+    {
+      ((unsigned char*)dest)[n] = ((unsigned char*)src)[n];
+      n--;
+    }
+  }
+  return (dest);
+}
