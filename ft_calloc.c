89b52505cc0dd9a05f02c636f022c16e4736591b
amasdouq <amasdouq@c4r8p1.1337.ma>
2023-11-24 01:49:02 +0100

    please i beg you

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: amasdouq <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 23:16:25 by amasdouq          #+#    #+#             */
-/*   Updated: 2023/11/22 21:50:38 by amasdouq         ###   ########.fr       */
+/*   Updated: 2023/11/24 01:48:18 by amasdouq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,24 @@
 
 void	*ft_calloc(size_t nelem, size_t elsize)
 {
-	void	*p;
+	char	*p;
 	size_t	total;
 
-	total = nelem * elsize;
-	if (nelem && ((total / nelem) != elsize))
-		return (NULL);
-	p = (void *)malloc(total);
-	if (!p)
-		return (NULL);
-	ft_bzero(p, total);
-	return (p);
+	if (nelem > 0 && elsize > 0)
+	{
+		total = nelem * elsize;
+		if (nelem && ((total / nelem) != elsize))
+			return (NULL);
+		p = (void *)malloc(total);
+		if (!p)
+			return (NULL);
+		ft_bzero(p, total);
+	}
+	else if (nelem == 0 || elsize == 0)
+	{
+		p = malloc(1);
+		p[0] = 0;
+		return ((void *)p);
+	}
+	return ((void *)p);
 }
